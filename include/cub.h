6b8dee34a8b2a0260c82efa245c8b01bd8540d80
sshahary <145033155+sshahary@users.noreply.github.com>
2024-05-25 17:48:57 +0200

    playing with textures

---
+++ include/cub.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sshahary <sshahary@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/21 13:54:12 by sshahary          #+#    #+#             */
+/*   Updated: 2024/05/25 17:25:47 by sshahary         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB_H
+# define CUB_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <math.h>
+# include "../lib/ft_printf/ft_printf.h"
+# include "../lib/gnl/get_next_line_bonus.h"
+# include "../MLX42/include/MLX42/MLX42.h"
+
+#define WIDTH 5120
+#define HEIGHT 2880
+
+
+#endif
--- main.c
+++ main.c
@@ -6,11 +6,17 @@
 /*   By: sshahary <sshahary@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 22:53:23 by sshahary          #+#    #+#             */
-/*   Updated: 2024/05/25 17:19:32 by sshahary         ###   ########.fr       */
+/*   Updated: 2024/05/25 17:47:29 by sshahary         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/cub.h"
+
+static void error(void)
+{
+	printf("%s", mlx_strerror(mlx_errno));
+	exit(EXIT_FAILURE);
+}
 
 void	init(mlx_t **mlx, mlx_image_t **img)
 {
@@ -46,13 +52,24 @@ int	main()
 	mlx_image_t* img;
 
 
+	mlx_texture_t* texture; 
+	
+	
 	init(&mlx, &img);;
+	texture = mlx_load_png("texture/brick.png");
+	if (!texture)
+		error();
+	img = mlx_texture_to_image(mlx, texture);
+	if (!img)
+		error();
+	if (mlx_image_to_window(mlx, img, 500, 600) < 0)
+		error();
 
 	mlx_loop_hook(mlx, loop_hook, mlx);
 	// mlx_key_hook(mlx, handle_keys, NULL);
 	mlx_loop(mlx);
-
 	mlx_delete_image(mlx, img);
+	mlx_delete_texture(texture);
 	mlx_terminate(mlx);
 	return 0;
 }
