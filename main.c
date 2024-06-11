f36fd2001fdb6a3afef218fc3b5ad6cbcfb86359
sshahary <145033155+sshahary@users.noreply.github.com>
2024-06-11 16:18:17 +0200

    changes and splits

--- main.c
+++ main.c
@@ -3,26 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asemsey <asemsey@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sshahary <sshahary@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 22:53:23 by sshahary          #+#    #+#             */
-/*   Updated: 2024/06/11 12:23:21 by asemsey          ###   ########.fr       */
+/*   Updated: 2024/06/11 15:20:26 by sshahary         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/cub.h"
-
-// static void error(void) {
-// 	printf("%s", mlx_strerror(mlx_errno));
-// 	exit(EXIT_FAILURE);
-// }
-
-void	ft_error(char *str)
-{
-	ft_putendl_fd("Error", 2);
-	ft_putendl_fd(str, 2);
-	exit(EXIT_FAILURE);
-}
 
 void	init(t_game game_data)
 {
@@ -30,21 +18,9 @@ void	init(t_game game_data)
 	game_data.player->y = game_data.cub->y * TILE + TILE / 2;
 	game_data.player->fovradian = FOV * M_PI / 180;
 	game_data.player->angle = M_PI;
-	// printf("x: %d\n, y: %d\n", game_data.player->x, game_data.player->y);
 }
 
-// void clear_image(mlx_image_t *img, uint32_t color)
-// {
-// 	for (uint32_t y = 0; y < img->height; y++)
-// 	{
-// 		for (uint32_t x = 0; x < img->width; x++)
-// 		{
-// 			((uint32_t*)img->pixels)[y * img->width + x] = color;
-// 		}
-// 	}
-// }
-
-void load_textures(t_game *data)
+void	load_textures(t_game *data)
 {
 	data->textures.north = mlx_load_png(data->cub->no_png);
 	data->textures.south = mlx_load_png(data->cub->so_png);
@@ -66,7 +42,8 @@ void load_textures(t_game *data)
 
 void	loop_hook(void* param)
 {
-	t_game *data;
+	t_game	*data;
+
 	data = param;
 	mlx_delete_image(data->mlx, data->img);
 	data->img = mlx_new_image(data->mlx, SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -74,23 +51,25 @@ void	loop_hook(void* param)
 	ray_cast(data);
 	mlx_image_to_window(data->mlx, data->img, 0, 0);
 }
-
-int main(int argc, char **argv)
+void	clear_stuff(t_game game_data)
+{
+	mlx_delete_image(game_data.mlx, game_data.img);
+	mlx_delete_texture(game_data.textures.north);
+	mlx_delete_texture(game_data.textures.south);
+	mlx_delete_texture(game_data.textures.east);
+	mlx_delete_texture(game_data.textures.west);
+	mlx_terminate(game_data.mlx);
+}
+int	main(int argc, char **argv)
 {
 	t_game game_data;
-	t_cub	*cub = NULL;
+	t_cub	*cub;
 
-	if (argc == 2)
-	{
-		cub = ft_calloc(1, sizeof(t_cub));
-		if (read_mapfile(argv[1], cub))
-			printf("map read :)\n");
-		else
-			ft_error("couldn't read map file");
-		print_map(cub->map, cub->map_width);
-	}
-	else
+	if (argc != 2)
 		ft_error("forget to give map .cub file :(");
+	cub = ft_calloc(1, sizeof(t_cub));
+	if (!read_mapfile(argv[1], cub))
+		ft_error("couldn't read map file");
 	game_data.cub = cub;
 	game_data.player = ft_calloc(1, sizeof(t_player));
 	game_data.ray = ft_calloc(1, sizeof(t_ray));
@@ -100,11 +79,6 @@ int main(int argc, char **argv)
 	mlx_loop_hook(game_data.mlx, &loop_hook, &game_data);
 	mlx_key_hook(game_data.mlx, &key, &game_data);
 	mlx_loop(game_data.mlx);
-	mlx_delete_image(game_data.mlx, game_data.img);
-	mlx_delete_texture(game_data.textures.north);
-	mlx_delete_texture(game_data.textures.south);
-	mlx_delete_texture(game_data.textures.east);
-	mlx_delete_texture(game_data.textures.west);
-	mlx_terminate(game_data.mlx);
+	clear_stuff(game_data);
 	return (EXIT_SUCCESS);
 }
